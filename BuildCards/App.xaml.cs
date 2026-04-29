6c4cc0bd077aee06249355ea00b00c52c4a77fbe
Ismail Benrahhal <ibenrahhal133@gmail.com>
2026-04-29 18:57:18 +0100

    Initial commit: WPF project with Helpers and Controls/Cards structure

---
+++ BuildCards/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BuildCards;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
