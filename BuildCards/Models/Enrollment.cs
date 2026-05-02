e01fbe0843f57109a33121949db70c52aec30bc5
Ismail Benrahhal <ibenrahhal133@gmail.com>
2026-05-02 13:53:32 +0100

    Add Enrollment model with computed properties

---
+++ BuildCards/Models/Enrollment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildCards.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public string StudentFullName { get; set; } = null!;
+        public string CourseTitle { get; set; } = null!;
+        public DateTime EnrollmentDate { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public decimal ProgressPercent { get; set; }
+        public decimal? FinalGrade { get; set; }
+        public string Status { get; set; } = null!;
+
+        // Computed
+        public string ProgressText => $"{ProgressPercent}%";
+        public string GradeText => FinalGrade.HasValue ? $"{FinalGrade:F1}" : "N/A";
+        public string EnrollmentDateFormatted => EnrollmentDate.ToString("dd MMM yyyy");
+    }
+}
